23c34132c75d2ecf5ac0cc1ec0c8b8743491db45
cristian-rincon <cristian.o.rincon.b>
2020-04-15 09:44:44 -0500

    Ejemplo de api rest con django terminado

---
+++ mysite/myapi/models.py
@@ -0,0 +1,8 @@
+# My api models
+from django.db import models
+
+class Hero(models.Model):
+    name = models.CharField(max_length=60)
+    alias = models.CharField(max_length=60)
+    def __str__(self):
+        return self.name
---
+++ mysite/myapi/serializers.py
@@ -0,0 +1,10 @@
+# Serializers
+
+from rest_framework import serializers
+
+from .models import Hero
+
+class HeroSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Hero
+        fields = ('name', 'alias')
